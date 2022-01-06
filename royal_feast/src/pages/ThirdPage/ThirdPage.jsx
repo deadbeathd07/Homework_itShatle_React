8d2e05e67879d260d06b6814c06895b74801288f
LebedevichAV <deadbeat.hd.07@gmail.com>
2022-01-06 14:08:02 +0300

    added styles

--- royal_feast/src/pages/ThirdPage/ThirdPage.jsx
+++ royal_feast/src/pages/ThirdPage/ThirdPage.jsx
@@ -3,10 +3,10 @@ import DATA from "../../DATA/DATA";
 import shuffle from "../../functions/shuffle";
 import { useSelector } from "react-redux";
 import { selectUsers } from "../../store/selectors";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Card from "../../components/Card/Card";
 import { Link } from "react-router-dom";
-import {resetStore} from "../../store/store";
+import { resetStore } from "../../store/store";
 import { useDispatch } from "react-redux";
 
 function ThirdPage() {
@@ -17,33 +17,42 @@ function ThirdPage() {
   const users = Array.from(useSelector(selectUsers));
   const [count, setCount] = useState(0);
 
+  useEffect(() => {
+    console.log("!!!");
+    handleClickChangeCount();
+  }, []);
+
   function handleClickChangeCount() {
+    console.log("!");
     setCount(count + 1);
   }
 
   function exit() {
+    console.log("!!");
     dispatch(resetStore());
   }
-
+  console.log("!!!!!!!");
   return (
     <div className="thirdPage">
-      <div>
-        {count < users.length ? (
-          <Card
-            key={count}
-            name={users[count].name}
-            exercise={newData[count].exercise}
-            func={handleClickChangeCount}
-          ></Card>
-        ) : (
-          setCount(0)
-        )}
+      <div className="cardWrap">
+        <Link to="/">
+          <button type="button" onClick={exit} className="exit">
+            X
+          </button>
+        </Link>
+        <div className="card">
+          {count < users.length ? (
+            <Card
+              key={count}
+              name={users[count].name}
+              exercise={newData[count].exercise}
+              func={handleClickChangeCount}
+            ></Card>
+          ) : (
+            setCount(0)
+          )}
+        </div>
       </div>
-      <Link to="/">
-        <button type="button" onClick={exit}>
-          exit
-        </button>
-      </Link>
     </div>
   );
 }
